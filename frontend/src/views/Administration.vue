5a06ed338a38562711295a47aabf2157f4a468bf
InsertDisc <31751462+InsertDisc@users.noreply.github.com>
2024-09-04 20:31:41 -0400

    Update Administration.vue
    Added openid-config component to view.

--- frontend/src/views/Administration.vue
+++ frontend/src/views/Administration.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import UsersTable from "@/components/Administration/Users/Table.vue";
 import Tasks from "@/layouts/Administration/Tasks.vue";
+import OpenIDConfig from "@/layouts/Administration/openid-config.vue";
 import storeAuth from "@/stores/auth";
 
 // Props
 const auth = storeAuth();
 </script>
+
 <template>
-  <users-table  />
+  <users-table />
   <tasks v-if="auth.scopes.includes('tasks.run')" />
+  <open-id-config />
 </template>
